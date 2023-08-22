3aec80a8494e6ee0efd3cb280ae28e9b04dc96b8
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-08-22 17:00:44 -0300

    feat: add findbyid and findall routes

--- src/infra/http/express/index.ts
+++ src/infra/http/express/index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import { RouteInMemoryRepository } from "@/infra/in-memory/route-in-memory.repository";
-import { CreateRouteUseCase } from "@/application/create-route.use-case";
+import { CreateRouteUseCase } from "@/use-cases/usecases-route/create";
 import { createRouteSchema } from "@/infra/lib/zodSchemas";
+import { FetchAllRouteUseCase } from "@/use-cases/usecases-route/fetch-all";
+import { FetchRouteByIdUseCase } from "@/use-cases/usecases-route/fetch-by-id";
 
 const app: Express = express();
 const inMemoRepository = new RouteInMemoryRepository();
@@ -23,4 +25,31 @@ app.post("/routes", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/routes", async (req: Request, res: Response) => {
+  try {
+    const findAllRouteUseCase = new FetchAllRouteUseCase(inMemoRepository);
+    const response = await findAllRouteUseCase.execute();
+
+    if (response) return res.status(200).json(response);
+
+    throw new Error();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+app.get("/routes/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const findbyIdRouteUseCase = new FetchRouteByIdUseCase(inMemoRepository);
+
+    const response = await findbyIdRouteUseCase.execute(id);
+
+    if (response) return res.status(200).json(response);
+
+    throw new Error();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
 app.listen(3000, () => console.log("Server is running on port 3000"));
